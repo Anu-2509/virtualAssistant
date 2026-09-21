27e91a0fdf74e6dbda392d565f46a92ce534067a
Anu Sahu <anusahu2509@gmail.com>
2026-09-21 15:46:44 +0530

    Change cookie settings for authentication
    Updated cookie settings to use 'None' for sameSite and enabled secure flag.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -31,8 +31,8 @@ const signUp = async (req,res) => {
         res.cookie("token",token,{
             httpOnly: true,
             maxAge: 7*24*60*60*1000,
-            sameSite: "strict",
-            secure: false
+            sameSite: "None",
+            secure: true
         })
 
         return res.status(201).json({
@@ -78,8 +78,8 @@ const login = async (req,res) => {
         res.cookie("token",token,{
             httpOnly: true,
             maxAge: 7*24*60*60*1000,
-            sameSite: "strict",
-            secure: false
+            sameSite: "None",
+            secure: true
         })
 
         return res.status(200).json({
@@ -102,8 +102,8 @@ const logout = async (req, res) => {
 
         res.clearCookie("token", {
             httpOnly: true,
-            sameSite: "strict",
-            secure: false
+            sameSite: "None",
+            secure: true
         });
 
         return res.status(200).json({
